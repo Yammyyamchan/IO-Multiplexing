59d79c290a22234d55cfe867959af4ec5dd97025
박찬석 <131407815+Yammyyamchan@users.noreply.github.com>
2024-01-01 23:34:21 +0900

    Update command_client.c

--- command_client.c
+++ command_client.c
@@ -43,23 +43,23 @@ int main(int argc,char**argv)
     while(1)
     {
         pos=0;
-        printf("Command count(0 to quit): ");
-        scanf("%d",&cnt);
+        printf("Command count(0 to quit): ");   //명령어 개수를 입력받는다.
+        scanf("%d",&cnt);                       //명령어 개수를 scanf()을 이용해 저장
         if(cnt==0)
-            break;
+            break;                              //명령어 개수가 0이면 프로그램 종료
     
-        msg[pos++]=(char)cnt;
+        msg[pos++]=(char)cnt;                   //입력받은 명령어 개수를 메시지의 첫번째 인덱스에 저장
     
         fgetc(stdin);
-        for(i=0;i<cnt;i++)
+        for(i=0;i<cnt;i++)                      //명령어 개수만큼 반복
         {
-            printf("command[%d]: ",i+1);
-            fgets(cmd,sizeof(cmd),stdin);
-            len=strlen(cmd);
-            --len;
-            msg[pos++]=(char)len;
-            strncpy(&msg[pos],cmd,len);
-            pos+=len;
+            printf("command[%d]: ",i+1);        //명령어를 입력한다.
+            fgets(cmd,sizeof(cmd),stdin);       //입력한 명령어를 cmd에 저장한다.
+            len=strlen(cmd);                    
+            --len;                              //fgets()함수로 입력해 저장해서 \n을 빼야됨.
+            msg[pos++]=(char)len;               // \n을 뺀 명령어의 길이를 메시지 인덱스에 저장
+            strncpy(&msg[pos],cmd,len);         // cmd의 명령어를 메시지에 저장함
+            pos+=len;                           //메시지의 인덱스 값을 증가시킴
         }
         write(sock,msg,strlen(msg));
     
